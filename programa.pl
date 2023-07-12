c937ff6181457760e0404df38184d071bafc3d4a
GregorioOndjian <gondjian@frba.utn.edu.ar>
2023-07-11 23:16:51 -0300

    Hasta el punto 3 terminado

--- programa.pl
+++ programa.pl
@@ -1,1 +1,44 @@
 % Aquí va el código.
+restaurante(panchoMayo, 2, barracas).
+restaurante(finoli, 3, villaCrespo).
+restaurante(superFinoli, 5, villaCrespo).
+
+restauranteActivo(Restaurante):-
+    menu(Restaurante,_).
+
+restauranteActivo(superFinoli).
+
+menu(panchoMayo, carta(1000, pancho)).
+menu(panchoMayo, carta(200, hamburguesa)).
+menu(finoli, carta(2000, hamburguesa)).
+menu(finoli, pasos(15, 15000, [chateauMessi, francescoliSangiovese, susanaBalboaMalbec], 6)).
+menu(noTanFinoli, pasos(2, 3000, [guinoPin, juanaDama],3)).
+
+vino( chateauMessi, francia, 5000).
+vino( francescoliSangiovese, italia, 1000).
+vino( susanaBalboaMalbec, argentina, 1200).
+vino( christineLagardeCabernet, argentina, 5200).
+vino( guinoPin, argentina, 500).
+vino( juanaDama, argentina, 1000).
+
+restaurantesPorLaZona(Restaurante,Estrellas,Zona):-
+    restaurante(Restaurante,CantEstrellas,Zona),
+    CantEstrellas > Estrellas.
+
+restauranteSinEstrellas(Restaurante):-
+    restauranteActivo(Restaurante),
+    not(restaurante(Restaurante,_,_)).
+
+malOrganizado(Restaurante):-
+    menu(Restaurante,Menu),
+    malaOrganizacionDeMenu(Restaurante,Menu).
+
+malaOrganizacionDeMenu(Restaurante,carta(Precio,Plato)):-
+    menu(Restaurante,carta(Precio,Plato)),
+    menu(Restaurante,carta(OtroPrecio,Plato)),
+    OtroPrecio \= Precio.
+
+malaOrganizacionDeMenu(_, pasos(CantidadDePasos,_,ListaVinos,_)):-
+    length(ListaVinos,NumeroVinos),
+    CantidadDePasos > NumeroVinos.
+
